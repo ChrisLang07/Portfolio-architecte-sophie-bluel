ac136edb95e9912ec74ae22f76ac3e6a19c25a12
ChrisLang07 <christophe.langlois.one@gmail.com>
2024-06-08 19:17:15 +0200

    Display all works from API

---
+++ FrontEnd/assets/js/login.js
@@ -0,0 +1,16 @@
+
+
+
+// connection aux ressources API //
+
+const url_login = "http://localhost:5678/api/users/login";
+
+const users = [
+    {
+        "username": "sophie",
+        "email": "sophie.bluel@test.tld",
+        "password": "S0phie",
+        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTcxNzY2NjIyNiwiZXhwIjoxNzE3NzUyNjI2fQ.UPrxwXjnlAP-HJZ5zYrnlk5fKOcJVwIC-oTaNumT5r0",
+    }
+];
+
--- FrontEnd/assets/js/scripts.js
+++ FrontEnd/assets/js/scripts.js
@@ -4,97 +4,104 @@ const url_works = "http://localhost:5678/api/works";
 const url_categories = "http://localhost:5678/api/categories";
 
 const node_gallery = document.querySelector("[rel=js-gallery]");
+const node_filters = document.querySelector("[rel=js-filters]");
+
+
+let works = [];
+let categories = [];
+
+/**
+ *
+ * Make a HTTP request and return an array
+ *  
+ * @param String url 
+ * @returns Array
+ */
 
 async function httpGet(url) {
     try {
         const response = await fetch(url);
-        const data = await response.json();
+        return await response.json();
 
-        return data;
     }
-
     catch (error) {
-        alert('HTTP Error ! :' + response.status);
+        alert("HTTP Error ! : " + response.status)
+        return [];
     }
-}
+};
 
-// Affichage de tous les projets //
+/**
+ * 
+ * @param String url 
+ * @param Object data, Data you nedd to pass to the request 
+ * @param Object headers, the HTTP request headers options 
+ * 
+ */
 
-async function workList() {
-    const works = await httpGet(url_works);
+async function httpPost(url, data, headers) {
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            body: data,
+            headers: headers
+        });
+        return await response.json();
+    }
+    catch (error) {
+        alert("HTTP Error : " + response.status)
+        return [];
+    }
+};
 
+/**
+ * Reset the .gallery element
+ */
+function resetGallery() {
     node_gallery.innerHTML = "";
+};
 
-    works.forEach(element => {
+/**
+ * 
+ * Create the HTML of work
+ * 
+ * @param Object work, a work data 
+ */
 
-        const figcaption = document.createElement("figcaption");
-        figcaption.textContent = element.title
+function createWork(work) {
+    
+    let figcaption = document.createElement("figcaption");
+        figcaption.textContent = work.title;
 
-        const img = document.createElement("img");
-        img.src = element.imageUrl;
-        img.alt = element.title
-
-        const figure = document.createElement("figure");
+    let img = document.createElement("img");
+        img.src = work.imageUrl;
+        img.alt = work.title;
+    
+    let figure = document.createElement("figure");
         figure.appendChild(img);
         figure.appendChild(figcaption);
 
-        node_gallery.appendChild(figure);
+    node_gallery.appendChild(figure);
+};
+        
 
-    })
-}
+/**
+ * 
+ * Add works on .gallery
+ * 
+ * @param array works 
+ */
 
-workList();
+function fillGallery(works) {
+    resetGallery();
+    works.forEach(work => createWork(work));
+};
 
-// Filtrage des projets par catégories //
-
-async function filter(id) {
-    const filter = await httpGet(url_works);
-    
-    node_gallery.innerHTML = "";
-    
-    filter.forEach(element => {
-        const category = element.category; 
-        const monSet = new Set();
-        monSet.add(category.name);
-        console.log(monSet);
-        if(monSet.has(id)) {
-            const figcaption = document.createElement("figcaption");
-            figcaption.textContent = element.title;
-    
-            const img = document.createElement("img");
-            img.src = element.imageUrl;
-            img.alt = element.title
-    
-            const figure = document.createElement("figure");
-            figure.appendChild(img);
-            figure.appendChild(figcaption);
-    
-            node_gallery.appendChild(figure);
-        }
-    })
-}
+(async () => {
+    works = await httpGet(url_works);
+    fillGallery(works);
+})();
 
 
 
-
-const filterAll = document.querySelector(".all");
-filterAll.addEventListener("click", function() {
-    workList();
-});
-
-const filterObjects = document.querySelector(".objects");
-filterObjects.addEventListener("click", function() {
-    filter("Objets");
-});
-
-const filterAppartment = document.querySelector(".appartments");
-filterAppartment.addEventListener("click", function() {
-    filter("Appartements");
-});
-
-const filterHotel = document.querySelector(".hotel");
-filterHotel.addEventListener("click", function() {
-    filter("Hotels & restaurants");
-});
 
 
