99f291665951201a14af83564597009d2cfaffd5
ChrisLang07 <christophe.langlois.one@gmail.com>
2024-06-12 11:03:19 +0200

    Login page working

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,16 +1,60 @@
-
-
-
-// connection aux ressources API //
-
 const url_login = "http://localhost:5678/api/users/login";
 
-const users = [
-    {
-        "username": "sophie",
-        "email": "sophie.bluel@test.tld",
-        "password": "S0phie",
-        "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTcxNzY2NjIyNiwiZXhwIjoxNzE3NzUyNjI2fQ.UPrxwXjnlAP-HJZ5zYrnlk5fKOcJVwIC-oTaNumT5r0",
+let form = document.querySelector('form');
+let email = document.querySelector('email');
+let password = document.querySelector('password');
+
+/**
+ * Make a HTTP POST Request and return an array
+ * 
+ * @param String url 
+ * @param Object data, Data you need to pass to the request
+ * @param Object headers, The HTTP request headers options
+ */
+async function httpPost(url, data, headers) {
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            body: data,
+            headers: headers
+        });
+        return await response.json();
     }
-];
+    catch (error) {
+        alert("HTTP Error : " + response.status)
+        return [];
+    }
+};
+
+
+async function login(url, data, headers) {
+    const login = await httpPost(url, data, headers);
+    console.log(login);
+    if(login.error) {
+        alert("Wrong Email and/or Password");
+    } else {
+        let storage = window.localStorage;
+        storage.setItem("token", login.token);       
+        window.location = "index.html";
+    }
+
+};
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+
+    let nodeEmail = event.target.email;
+    let nodePassword = event.target.password;
+    let data = JSON.stringify({
+        email: nodeEmail.value,
+        password: nodePassword.value
+    });
+    
+    let headers = {"Content-Type": "application/json",};
+    
+    login(url_login, data, headers);
+    
+});
+
+
 
--- FrontEnd/assets/js/scripts.js
+++ FrontEnd/assets/js/scripts.js
@@ -7,35 +7,33 @@ const node_gallery = document.querySelector("[rel=js-gallery]");
 const node_filters = document.querySelector("[rel=js-filters]");
 
 let works = [];
-let categories = [];
 
 /**
- *
- * Make a HTTP request and return an array
- *  
+ * Make a HTTP GET Request and return an array
+ * 
  * @param String url 
  * @returns Array
  */
-
 async function httpGet(url) {
     try {
         const response = await fetch(url);
         return await response.json();
-
     }
     catch (error) {
-        alert("HTTP Error ! : " + response.status)
+        alert('HTTP Error ! :' + response.status);
         return [];
     }
-};
+}
 
 /**
-data, Data you nedd to pass to the request 
- * @param Object headers, the HTTP request headers options 
+ * Make a HTTP POST Request and return an array
  * 
+ * @param String url 
+ * @param Object data, Data you need to pass to the request
+ * @param Object headers, The HTTP request headers options
  */
-
-async function httpPost(url, data, headers) {
+async function httpPost(url, data, headers)
+{
     try {
         const response = await fetch(url, {
             method: 'POST',
@@ -45,95 +43,106 @@ async function httpPost(url, data, headers) {
         return await response.json();
     }
     catch (error) {
-        alert("HTTP Error : " + response.status)
+        alert('HTTP Error ! :' + response.status);
         return [];
     }
-};
+}
 
 /**
- * Reset the .gallery element
+ * Reset the .gallery elements
  */
-function resetGallery() {
+function resetGallery()
+{
     node_gallery.innerHTML = "";
-};
+}
 
 /**
+ * Add works on .gallery
  * 
- * Create the HTML of work
- * 
- * @param Object work, a work data 
+ * @params Array works
  */
+function fillGallery(works)
+{
+    resetGallery();
+    works.forEach(work => createWork(work));
+}
 
-function createWork(work) {
-    
+/**
+ * Create the HTML of a work
+ * 
+ * @param Object work, a work data
+ */
+function createWork(work)
+{
     let figcaption = document.createElement("figcaption");
-        figcaption.textContent = work.title;
+        figcaption.textContent = work.title
 
     let img = document.createElement("img");
         img.src = work.imageUrl;
-        img.alt = work.title;
-    
+        img.alt = work.title
+
     let figure = document.createElement("figure");
         figure.appendChild(img);
         figure.appendChild(figcaption);
 
     node_gallery.appendChild(figure);
-};
+}
 
-/**
- * 
- * create the works of a category
- * 
- * @param category, the category of a work 
- */
-
-function createFilters(category) {
-    
+function createFilter(filter)
+{
     let li = document.createElement('li');
-    li.classList.add("filter-tag");
-    li.textContent = category.name;
-    li.dataset.category = category.id;
-    
-    li.addEventListener('click', event => {
-        
-        let node = event.target;
-        console.log(node);
-        let categoryId = node.dataset.category;
-        let filteredWorks = works.filter(work => work.categoryId == categoryId);
-        categoryId == 0? fillGallery(works) : fillGallery(filteredWorks);
-        
-    });
-    
-    node_filters.appendChild(li);
-};
+        li.classList.add('filter-tag');
+        li.dataset.category = filter.id;
+        li.textContent = filter.name;
 
-function fillFilters(categories) {
-    categories.forEach(category => createFilters(category)); 
+        li.addEventListener('click', event => {
+
+            let node = event.target;
+            let categoryId = node.dataset.category;
+            let filteredWorks = works;
+            
+            resetActiveFilter(categoryId);
+
+            if (categoryId != 0)
+            {
+                filteredWorks= works.filter(work => work.categoryId == categoryId);
+            }
+            
+            fillGallery(filteredWorks);
+
+        })
     
-};      
+    node_filters.append(li);
+}
 
-/**
- * 
- * Add works on .gallery
- * 
- * @param array works 
- */
+function resetActiveFilter(activeId)
+{
+    const filters = node_filters.querySelectorAll('.filter-tag');
 
-function fillGallery(works) {
-    resetGallery();
-    works.forEach(work => createWork(work));
-};
+    filters.forEach(filter => filter.classList.remove('active'));
+
+    filters.forEach(filter => {
+        if (filter.dataset.category == activeId)
+        {
+            filter.classList.add('active')
+        }
+    })
+}
+
+
 
 (async () => {
-    // Get data
+    // Get Data
+    const categories = await httpGet(url_categories);
     works = await httpGet(url_works);
-    categories = await httpGet(url_categories);
-    
-    createFilters({id: 0, name: 'Tous'});
-    fillFilters(categories);
-    
+
+    // Create Categories
+    createFilter({id: 0, name: 'Tous'});
+    categories.forEach(category => createFilter(category));
+
+    // Create Works
     fillGallery(works);
-    
+    resetActiveFilter(0);
 })();
 
 
